d69d971b057d2e76758d8235a1c437aafdb16e00
Julien <julientora@gmail.com>
2018-04-27 16:09:45 +0200

    Modif commande boissons

--- themes/lesdejsurlherbe/functions.php
+++ themes/lesdejsurlherbe/functions.php
@@ -202,9 +202,9 @@ function add_drinks_products_to_cart() {
    'category' => array( 'boissons' ),
 	);
 	$products = wc_get_products( $args );
+	echo '<p>Avez-vous pensé aux boissons ?</p>';
 	foreach ($products as $indiv_product) {
-		echo 'Avez-vous pensé aux boissons ?';
-		echo '<div class="thumb">'.$indiv_product->get_image().'</div>';
+		//echo '<div class="thumb">'.$indiv_product->get_image().'</div>';
 		echo '<div class="product-title">'.$indiv_product->get_title().'</div';
 		echo '<div class="product-price">'.$indiv_product->get_price().'</div';
 		woocommerce_quantity_input();
